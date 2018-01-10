1b85c58af7e9299e57539bd8222bc8b500730637
TaehongKim <ttahong2@gmail.com>
2018-01-10 11:32:34 +0900

    10th_Jan_2nd update

---
+++ Code_and_Schematic/Wemos_D1/LCD Display/lcd_16x2__Temperature_sensor/lcd_16x2__Temperature_sensor.ino
@@ -0,0 +1,43 @@
+#include <DHT11.h>    //라이브러리 불러옴
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+int pin=13;            //Signal 이 연결된 아두이노의 핀번호
+DHT11 dht11(pin);      
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+  
+void setup()
+{
+   Serial.begin(9600); //통신속도 설정
+   lcd.begin();
+
+  // Turn on the blacklight and print a message.
+   lcd.backlight();
+}
+ 
+void loop()
+{
+  int err;
+  float temp, humi;
+  if((err=dht11.read(humi, temp))==0) //온도, 습도 읽어와서 표시
+  {
+    Serial.print("temperature:");
+    Serial.print(temp);
+    Serial.print(" humidity:");
+    Serial.print(humi);
+    Serial.println();
+     
+    lcd.setCursor(0,0);
+    lcd.print("T:");
+    lcd.print(temp);
+  }
+  else                                //에러일 경우 처리
+  {
+    Serial.println();
+    Serial.print("Error No :");
+    Serial.print(err);
+    Serial.println();    
+  }
+  delay(1000);                        //1초마다 측정
+}
+ 
---
+++ Code_and_Schematic/Wemos_D1/Relay_module/Relay_module/Relay_module.ino
@@ -0,0 +1,29 @@
+const int BUTTON1 = 5;
+const int ledPin =  16; 
+
+int buttonState = 0;
+int flag=0;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(ledPin,OUTPUT);
+  pinMode(BUTTON1,INPUT);
+}
+
+void loop()
+{
+  Serial.println( );
+  if(digitalRead(BUTTON1) == HIGH)
+  { 
+    Serial.println("Button1 1");
+    digitalWrite(ledPin, HIGH);
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW);
+    Serial.println("Button1 0");
+  } 
+  delay(200);
+}
